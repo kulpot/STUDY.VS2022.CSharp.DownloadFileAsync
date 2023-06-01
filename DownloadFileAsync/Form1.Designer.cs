c1fb301c7ab195e17142c8b541a4cef932ddc6db
Kulpot <kulpotwahu@gmail.com>
2023-06-02 07:33:42 +0800

    Add project files.

---
+++ DownloadFileAsync/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DownloadFileAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 99);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(383, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(63, 12);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(332, 20);
+            this.txtAdress.TabIndex = 1;
+            this.txtAdress.TextChanged += new System.EventHandler(this.txtAdress_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File Name";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(69, 38);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(326, 20);
+            this.txtFilename.TabIndex = 3;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(266, 64);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(129, 23);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 134);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ DownloadFileAsync/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// ref link:https://www.youtube.com/watch?v=fPD2KwwZsjs&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=70
+// Use the WebClient to download a file asynchronously.
+// leopus.weebly.com/uploads/4/5/9/1/4591803bsf_file_finder_1.0.1_src.zip
+
+namespace DownloadFileAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAdress_TextChanged(object sender, EventArgs e)
+        {
+            txtFilename.Text = Path.GetFileName(txtAdress.Text);
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            btnDownload.Enabled = false; // disabled button more than once while its downloading
+            progressBar.Value = 0; // resets when initiates another download
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(webClient_DownloadFileCompleted);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(webClient_DownloadProgressChanged);
+            webClient.DownloadFileAsync(new Uri(txtAdress.Text), txtFilename.Text);
+
+        }
+
+        private void webClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Error == null)
+            {
+                MessageBox.Show("Download successfully completed!");
+            }
+            else
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+
+            ((WebClient)sender).Dispose();
+
+            btnDownload.Enabled = true; // re-enable button when download is completed
+        }
+
+        private void webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+    }
+}
